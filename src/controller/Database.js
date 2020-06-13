f671254eadc765fb890865fd88f6d1379f9c1dea
micmine <micmine4@gmail.com>
2020-06-13 21:06:01 +0200

    :racehorse: Single Database Connenction

---
+++ src/controller/Database.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose default connection open to ' + process.env.DATABASE);
+});
+
+// If the connection throws an error
+mongoose.connection.on('error',function (err) {
+  console.log('Mongoose default connection error: ' + err);
+});
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose default connection disconnected');
+});
+
+// If the Node process ends, close the Mongoose connection
+process.on('SIGINT', function() {
+  mongoose.connection.close(function () {
+    console.log('Mongoose default connection disconnected through app termination');
+    process.exit(0);
+  });
+});
+
